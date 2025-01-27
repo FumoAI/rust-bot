af65ff081f1b82fdf1957ab90ce0aab28213e013
Zecyel <i@zecyel.xyz>
2025-01-27 15:24:42 +0800

    feat: try getting the nickname of the sender

--- plugins/hi/src/lib.rs
+++ plugins/hi/src/lib.rs
@@ -6,7 +6,11 @@ async fn main() {
     plugin::on_msg(|event| async move {
         if event.borrow_text() == Some("摸摸") {
             if random::<f32>() < 0.75 {
-                event.reply("喵呜~");
+                if let Some(nickname) = event.sender.nickname.as_ref() {
+                    event.reply(format!("摸摸 {}", nickname));
+                } else {
+                    event.reply("喵呜~");
+                }
             } else {
                 event.reply("( *・ω・)✄╰ひ╯");
             }
