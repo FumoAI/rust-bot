a16ffa82dc97f40411265a7101d4fe09b6c6c598
_Kerman <kermanx@qq.com>
2025-01-27 18:12:51 +0800

    refactor: extract handler as a crate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use kovi::build_bot;
-use rust_bot::group_handler::{Counter, GroupHandler};
 
 fn main() {
-    let mut bot = build_bot!(hi, ai, kovi_plugin_title, kovi_plugin_cmd);
-    Counter::mount_on(&mut bot);
+    let bot = build_bot!(hi, ai, kovi_plugin_title, kovi_plugin_cmd);
     bot.run();
 }
