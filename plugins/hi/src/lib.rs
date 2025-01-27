276011b02ef6f398860598292c5c5bfd43c26e02
Zecyel <i@zecyel.xyz>
2025-01-27 15:12:12 +0800

    feat: add random to hi plugin

--- plugins/hi/src/lib.rs
+++ plugins/hi/src/lib.rs
@@ -1,10 +1,15 @@
 use kovi::PluginBuilder as plugin;
+use rand::random as random;
 
 #[kovi::plugin]
 async fn main() {
     plugin::on_msg(|event| async move {
-        if event.borrow_text() == Some("hi") {
-            event.reply("Miao~")
+        if event.borrow_text() == Some("摸摸") {
+            if random::<f32>() < 0.75 {
+                event.reply("喵呜~");
+            } else {
+                event.reply("( *・ω・)✄╰ひ╯");
+            }
         }
     });
 }
