387a5b42734c435f3b070746cb0a8cf5bb821c13
_Kerman <kermanx@qq.com>
2025-01-27 17:52:19 +0800

    chore: update

--- src/group_handler.rs
+++ src/group_handler.rs
@@ -8,23 +8,26 @@ pub struct GroupContext {
     group_id: i64,
 }
 
-impl GroupContext {
-    pub fn send_msg<T>(&self, message: T)
-    where
-        Message: From<T>,
-        T: Serialize,
-    {
-        self.bot.send_group_msg(self.group_id, message);
-    }
-}
-
 pub trait GroupHandler: Send + Sync + Sized + 'static {
     const NAME: &'static str = "<unnamed>";
     const VERSION: &'static str = "0.0.1";
 
     fn new(context: GroupContext) -> Self;
+    fn context(&self) -> &GroupContext;
+    fn bot(&self) -> Arc<RuntimeBot> {
+        self.context().bot.clone()
+    }
+    fn group_id(&self) -> i64 {
+        self.context().group_id
+    }
 
-    fn on_msg(&mut self, message: Arc<MsgEvent>) -> impl Future<Output = ()> + Send;
+    fn send_msg<T>(&self, message: T)
+    where
+        Message: From<T>,
+        T: Serialize,
+    {
+        self.bot().send_group_msg(self.group_id(), message);
+    }
 
     fn mount_on(bot: &mut Bot) {
         async fn setup<T: GroupHandler + 'static>() {
@@ -59,6 +62,8 @@ pub trait GroupHandler: Send + Sync + Sized + 'static {
             Arc::new(move || Box::pin(setup::<Self>())),
         )
     }
+
+    fn on_msg(&mut self, message: Arc<MsgEvent>) -> impl Future<Output = ()> + Send;
 }
 
 pub struct Counter {
@@ -72,20 +77,22 @@ impl GroupHandler for Counter {
     fn new(context: GroupContext) -> Self {
         Self { context, count: 0 }
     }
+    fn context(&self) -> &GroupContext {
+        &self.context
+    }
 
     async fn on_msg(&mut self, message: Arc<MsgEvent>) {
         if let Some(text) = message.borrow_text() {
             if text.starts_with("%+") {
                 if let Ok(num) = text[2..].trim().parse::<u64>() {
                     self.count += num;
-                    self.context
-                        .send_msg(format!("Counter increased to {}", self.count));
+                    self.send_msg(format!("Counter increased to {}", self.count));
                 } else {
-                    self.context.send_msg("Invalid number");
+                    self.send_msg("Invalid number");
                 }
             } else if text == "%reset" {
                 self.count = 0;
-                self.context.send_msg("Counter reset");
+                self.send_msg("Counter reset");
             }
         }
     }
